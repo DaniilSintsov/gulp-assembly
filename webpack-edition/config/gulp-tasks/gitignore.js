1b7a2610daf0a65eba762be80a8528770bbf0bf4
Daniil Sintsov <daniilpanfilov519@gmail.com>
2022-06-22 12:13:11 +0300

    add webpack edition

---
+++ webpack-edition/config/gulp-tasks/gitignore.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+
+export const gitignore = () => {
+  if (!fs.existsSync('.gitignore')) {
+    fs.writeFile('./.gitignore', '', cb)
+    fs.appendFile('./.gitignore', '**/phpmailer\r\n', cb)
+    fs.appendFile('./.gitignore', 'package-lock.json\r\n', cb)
+    fs.appendFile('./.gitignore', 'node_modules/\r\n', cb)
+    fs.appendFile('./.gitignore', '.gitignore\r\n', cb)
+    fs.appendFile('./.gitignore', 'dist/\r\n', cb)
+    fs.appendFile('./.gitignore', 'version.json\r\n', cb)
+    fs.appendFile('./.gitignore', 'config/gulp-settings.js\r\n', cb)
+    fs.appendFile('./.gitignore', '**/video/\r\n', cb)
+    fs.appendFile('./.gitignore', '**/*.zip\r\n', cb)
+    fs.appendFile('./.gitignore', '**/*.rar\r\n', cb)
+  }
+  return app.gulp.src(`${app.path.srcFolder}`)
+}
+
+function cb() {
+}
